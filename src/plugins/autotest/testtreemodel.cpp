qt-creator/qt-creator

AutoTest: Mark method as static
Change-Id: I14f2f0acf266a0fb4f88e26f50cfbe2257e3ab9a
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/autotest/testtreemodel.cpp
@@ -213,11 +213,11 @@ QList<ITestConfiguration *> TestTreeModel::getTestsForFile(const Utils::FilePath
     return result;
 }
 
-QList<TestTreeItem *> TestTreeModel::testItemsByName(TestTreeItem *root, const QString &testName)
+static QList<TestTreeItem *> testItemsByName(TestTreeItem *root, const QString &testName)
 {
     QList<TestTreeItem *> result;
 
-    root->forFirstLevelChildItems([&testName, &result, this](TestTreeItem *node) {
+    root->forFirstLevelChildItems([&testName, &result](TestTreeItem *node) {
         if (node->type() == TestTreeItem::TestSuite || node->type() == TestTreeItem::TestCase) {
             if (node->name() == testName) {
                 result << node;
@@ -299,7 +299,7 @@ QList<TestTreeItem *> TestTreeModel::testItemsByName(const QString &testName)
 {
     QList<TestTreeItem *> result;
     for (TestTreeItem *frameworkRoot : frameworkRootNodes())
-        result << testItemsByName(frameworkRoot, testName);
+        result << Autotest::testItemsByName(frameworkRoot, testName);
 
     return result;
 }

shentok/qtcreator

(cherry picked from commit cba4d5de5c24bd07a70c6c5285f1d91f8b65ee17)

src/plugins/autotest/testtreemodel.cpp
@@ -190,11 +190,11 @@ QList<TestConfiguration *> TestTreeModel::getTestsForFile(const Utils::FilePath 
     return result;
 }
 
-QList<TestTreeItem *> TestTreeModel::testItemsByName(TestTreeItem *root, const QString &testName)
+static QList<TestTreeItem *> testItemsByName(TestTreeItem *root, const QString &testName)
 {
     QList<TestTreeItem *> result;
 
-    root->forFirstLevelChildren([&testName, &result, this](TestTreeItem *node) {
+    root->forFirstLevelChildren([&testName, &result](TestTreeItem *node) {
         if (node->type() == TestTreeItem::TestSuite || node->type() == TestTreeItem::TestCase) {
             if (node->name() == testName) {
                 result << node;
@@ -218,7 +218,7 @@ QList<TestTreeItem *> TestTreeModel::testItemsByName(const QString &testName)
 {
     QList<TestTreeItem *> result;
     for (Utils::TreeItem *frameworkRoot : *rootItem())
-        result << testItemsByName(static_cast<TestTreeItem *>(frameworkRoot), testName);
+        result << Autotest::testItemsByName(static_cast<TestTreeItem *>(frameworkRoot), testName);
 
     return result;
 }